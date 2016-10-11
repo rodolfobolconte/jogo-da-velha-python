e8f4251c446a810d883c08579a6e8f0e36034c76
Rodolfo Bolconte <rodolfobolconte@gmail.com>
2016-10-11 19:22:09 -0300

    Rodando o jogo se O comecar primeiro

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -69,3 +69,18 @@ if comeco=='x':
             if compara('X')==0:
                 print('Velha!!!')
     imprime()
+
+else:
+    for i in range(5):
+        imprime() ; vezdeO()
+        if compara('O')==1:
+            break
+        imprime() ; vezdeX()
+        if compara('X')==1:
+            break
+        if i==4:
+            imprime() ; vezdeX()
+            if compara('O')==0:
+                print('Velha!!!')
+    imprime()
+
