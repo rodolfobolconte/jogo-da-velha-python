633c20341c601d78592adc026dc2deb054989e29
suelzinho121 <maxsuelmedeiros@hotmail.com>
2016-10-18 16:31:47 -0300

    Update cliente_velha.py

--- cliente_velha.py
+++ cliente_velha.py
@@ -3,13 +3,16 @@
 #http://www.scriptbrasil.com.br/forum/topic/96896-socket-em-python-como-interagir-enfim-resolvido/
 #Primeira função para utilizar o socket que nesse caso é cliente jogo da velha
 import socket
-HOST = '192.168.1.10'  # Endereco IP do Servidor
+ip_maquina=input("Digite o endereço ip da maquina")
 PORT = 5000            # Porta que o Servidor esta
 cliente_velha = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dest = (HOST, PORT)
-print 'Para sair use CTRL+X\n'
+print ("Para sair use CTRL+X\n")
+#essa variavel "msg" é a mensagem que vai ser enviada, ou seja a posição 
+#que vai ser jogado, então qualquer coisa tem que modificar ou só atruibir 
+#para msg o valor da outra variavel e retirar o raw_input
 msg = raw_input()
-while msg <> '\x18':
+while msg <> ("\x18"):
     cliente_velha.sendto (msg, dest)
     msg = raw_input()
 cliente_velha.close()
