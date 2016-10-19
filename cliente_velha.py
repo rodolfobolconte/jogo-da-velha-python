a0dc71728b3a091d9b44fcbeed8abbf9ec8b0396
Suelzinho121 <maxsuelmedeiros@hotmail.com>
2016-10-19 15:01:12 -0300

    Esses são os codigos do client e servidor que ainda devem ser configurados para receber as variaveis de jogadas

--- cliente_velha.py
+++ cliente_velha.py
@@ -2,17 +2,15 @@
 #http://wiki.python.org.br/SocketBasico
 #http://www.scriptbrasil.com.br/forum/topic/96896-socket-em-python-como-interagir-enfim-resolvido/
 #Primeira função para utilizar o socket que nesse caso é cliente jogo da velha
-import socket
-ip_maquina=input("Digite o endereço ip da maquina")
-PORT = 5000            # Porta que sera utilizada para acessar o servidor, pode ser alterada para outro valor
-cliente_velha = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-dest = (HOST, PORT)
-print ("Para sair use CTRL+X\n")
-#essa variavel "msg" é a mensagem que vai ser enviada, ou seja a posição 
-#que vai ser jogado, então qualquer coisa tem que modificar ou só atruibir 
-#para msg o valor da outra variavel e retirar o raw_input
-msg = raw_input()
-while msg <> ("\x18"):
-    cliente_velha.sendto (msg, dest)
-    msg = raw_input()
-cliente_velha.close()
+from socket import *
+ip_servidor=input("Digite o endereço ip da maquina servidor, se for uma conexão local so precisa digitar logalhost")
+porta_de_conexao=49250        # Porta que sera utilizada para acessar o servidor, pode ser alterada para outro valor
+servidor_velha=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor_velha.connect((ip_servidor,porta_de_conexao))
+jogada= #Essa variavel vai receber o local onde foi feito a jogada, então que que a cada jogada essa variavel receber a nova jogada
+for line in jogada:
+    servidor_velha.send(jogada) #isso é pra enviar a jogada para o servidor.
+    data=servidor_velha.recv(1024) # recebe o retorno da mensagem, que nesse caso é bom configurar para ser a jogada da outra maquina
+    print "A jogada do outro jogador foi na :",repr(data)
+servidor_velha.close()
+
--- servidor_velha.py
+++ servidor_velha.py
@@ -1,10 +1,15 @@
-import socket
-HOST = ''              # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-servidor_velha = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-orig = (HOST, PORT)
-servidor_velha.bind(orig)
+from socket import *
+ip_da_maquina_servidor='192.168.25.28' # Endereco IP do Servidor, deve ser setado caso esteja em redes diferentes, ou como localhost se estiver na rede local ou seja mesma maquina nesse caso
+porta_de_conexao=49250  # Porta que o Servidor esta conetando
+servidor_velha = socket(socket.AF_INET, socket.SOCK_DGRAM) # serve para criar um objeto socket tcp 
+servidor_velha.bind((ip_da_maquina_servidor,porta_de_conexao)) # com esse comando liga-se o servidor ao número da porta
+servidor_velha.listen(1) # serve para definir o numero maximo de conexoes que nesse caso so uma
 while True:
-    msg, cliente = servidor_velha.recvfrom(1024)
-    print cliente, msg
-servidor_velha.close()
+    connection, address=servidor_velha.accept()
+    print "existe uma conexao com ", address
+    while True:
+        data=connection.recv(1024)
+        print date
+        if not data: break
+        connection.send("Recebi isso :"+data)
+    connection.close()
