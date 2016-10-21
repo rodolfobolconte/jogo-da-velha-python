0e85cad0bc0ee64ebc9577a677fdc4716867dd07
Rodolfo Bolconte <rodolfobolconte@gmail.com>
2016-10-20 21:25:24 -0300

    consertando erro de compilação devido as importações

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -1,4 +1,5 @@
-﻿from random import choice
+﻿from random import randint
+from random import choice
 
 jogo=[[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 
@@ -66,8 +67,6 @@ def compara(jog):
     elif jogo[2][0]==jog and jogo[1][1]==jog and jogo[0][2]==jog: print('\n%c Ganhou!!!'%jog) ; return 1
 
     return 0
-
-from random import randint
 
 def IA(marcaPlayer, marcaPC):
     cont = 0
