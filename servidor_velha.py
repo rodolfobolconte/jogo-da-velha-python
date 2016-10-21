6fa295f6a42db03f97bd5703fca93d3db29596e6
MiqueasGaldino <miqueasgaldino@gmail.com>
2016-10-21 19:43:52 -0300

    Modificando código do cservidor socket

--- servidor_velha.py
+++ servidor_velha.py
@@ -1,15 +1,16 @@
-from socket import *
-ip_da_maquina_servidor='192.168.25.28' # Endereco IP do Servidor, deve ser setado caso esteja em redes diferentes, ou como localhost se estiver na rede local ou seja mesma maquina nesse caso
-porta_de_conexao=49250  # Porta que o Servidor esta conetando
-servidor_velha = socket(socket.AF_INET, socket.SOCK_DGRAM) # serve para criar um objeto socket tcp 
-servidor_velha.bind((ip_da_maquina_servidor,porta_de_conexao)) # com esse comando liga-se o servidor ao número da porta
-servidor_velha.listen(1) # serve para definir o numero maximo de conexoes que nesse caso so uma
+import socket
+HOST = "192.168.1.124"       # Endereco IP do Servidor
+PORT = 5000            # Porta que o Servidor esta
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orig = (HOST, PORT)
+tcp.bind(orig)
+tcp.listen(1)
 while True:
-    connection, address=servidor_velha.accept()
-    print "existe uma conexao com ", address
+    con, cliente = tcp.accept()
+    print ('Concetado por', cliente)
     while True:
-        data=connection.recv(1024)
-        print date
-        if not data: break
-        connection.send("Recebi isso :"+data)
-    connection.close()
+        msg = con.recv(1024)
+        if not msg: break
+        print (cliente, msg)
+    print ('Finalizando conexao do cliente', cliente)
+    con.close()
