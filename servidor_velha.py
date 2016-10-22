a591cb2ea9736f6fabb4c47b135c974dce2480d5
MiqueasGaldino <miqueasgaldino@gmail.com>
2016-10-21 23:23:00 -0300

    Adicionando versão final da conexão de redes do servidor socket

--- servidor_velha.py
+++ servidor_velha.py
@@ -1,16 +1,28 @@
-import socket
-HOST = "192.168.1.124"       # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-tcp.bind(orig)
-tcp.listen(1)
-while True:
-    con, cliente = tcp.accept()
-    print ('Concetado por', cliente)
-    while True:
-        msg = con.recv(1024)
-        if not msg: break
-        print (cliente, msg)
-    print ('Finalizando conexao do cliente', cliente)
-    con.close()
+from socket import *
+from time import time, ctime
+
+HOST = '192.168.0.104'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+while 1:
+    print ("Aguardando conexao...")
+    tcpClisock, addr = tcpSerSock.accept()
+    print ("...conexao de :"), addr
+    while 1:
+    	data = tcpClisock.recv(BUFSIZ)
+    	print ("[+] %s") % (data)
+    	mensagem=raw_input('Mensagem do servidor :')
+    	if data=="exit": 
+    		exit
+    	tcpClisock.send(mensagem)
+tcpClisock.close()
+exit
+tcpClicksock.close()
+
+
